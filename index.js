93db6164fe6af44d1c0d9da5e85276a3261a267d
Makis Tracend <makis.tracend@gmail.com>
2013-03-21 01:17:00 -0700

    #15 creating lib folder

---
+++ index.js
@@ -0,0 +1,11 @@
+/* 
+ * Drone Server
+ * Source: https://github.com/makesites/drone
+ * Author: Makis Tracend (@tracend)
+ * Released under the MIT license (http://makesites.org/licenses/MIT)
+ * Distributed through Makesites.org
+ */ 
+
+// Setup main app/server
+module.exports = require('./lib/cluster');
+
---
+++ server.js
@@ -0,0 +1,5 @@
+// Include important JS helpers
+var drone = require('./index'); 
+	
+// Listen on this port
+//server.listen(config.port); 
