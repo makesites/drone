e0d21f651194999e231d9dbdf22d72b665f693a1
Makis Tracend <makis.tracend@gmail.com>
2014-03-17 03:38:32 -0700

    :zzz: nw

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 //
-var config = require( __dirname +'/config/drone'), 
-	drone = require('./index'); 
-	
+var config = require( __dirname +'/config/drone'),
+	drone = require('./index');
+
 // Listen on activated ports
 if( drone.http ){
-	drone.http.listen(config.ports.http); 
-} 
+	drone.http.listen(config.ports.http);
+}
 if( drone.https ){
-	drone.https.listen(config.ports.ssl); 
+	drone.https.listen(config.ports.ssl);
 }
 
-//server.listen(config.port); 
+//server.listen(config.port);
